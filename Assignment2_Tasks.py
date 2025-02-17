66a10eea41fea85e603ebf9dd83a228b853bebc0
adam-rae <adam.rae@btinternet.com>
2025-02-17 15:57:53 +0000

    Assignment_2 now VectorClass, added Tasks file
    Renamed Assignment_2 to VectorClass as this name made more sense as an imported class, made a new file to handle the assigned tasks using the class

---
+++ Assignment2_Tasks.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 15:45:09 2025
+
+@author: adamr
+"""
+
+from VectorClass import Vector, PolarVector
+
+X = Vector(1,0,0)
+Y_P = PolarVector(1,0,90)
+print(X)
+print(Y_P)
