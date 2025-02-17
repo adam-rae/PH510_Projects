c390d8c5d649fd2b9b016d89c23d05eb47caa5a3
adam-rae <adam.rae@btinternet.com>
2025-02-17 15:43:40 +0000

    Update Assignment2.py
    Added working polar coordinate subclass with working conversions

--- Assignment_2.py
+++ Assignment_2.py
@@ -8,10 +8,11 @@ Created on Sun Feb 16 20:21:50 2025
 """
    Object oriented class of vectors
 """
+import numpy as np
 
 class Vector:
     """
-       Vector class for three dimensional quantities
+       Vector class for three dimensional quantities using Cartesian coordiantes
     """
     def __init__(self, i, j, k):
         self.i = i
@@ -40,30 +41,74 @@ class Vector:
     
     def magnitude(self):
         """
-        Parameters
+        Computes the magnitude of an instance as the square root of the sum of the squares of the vector components
         
         """
-        return (self.i**2 + self.j**2 + self.k**2)**(1/2)
+        return np.sqrt(self.i**2 + self.j**2 + self.k**2)
     
     def dot(self,other):
         """
-        Computes the dot product of two instances
+        Computes the dot product of two vector instances
         
         """
         return self.i * other.i + self.j * other.j + self.k * other.k
     
     def cross(self,other):
         """
-        Computes the cross product of two instances
+        Computes the cross product of two vector instances
 
         """
         return Vector(self.j * other.k - self.k * other.j,
                       -(self.i * other.k - self.k * other.i),
                       self.i * other.j - self.j * other.i)
     
+class PolarVector(Vector):
+    """
+       Vector class for three dimensional quantities using spherical/polar coordinates in degrees
+    """
+    def __init__(self, r, theta, phi):
+        theta = np.deg2rad(theta)
+        phi = np.deg2rad(phi)
+        Vector.__init__(self,
+                        r*np.cos(theta) * np.cos(phi),
+                        r*np.cos(theta) * np.sin(phi),
+                        r*np.sin(theta))
+        
+    def r(self):
+        """
+        Computes the radius component
+        """
+        return self.magnitude()
+    
+    def phi(self):
+        """
+        Computes the azimuthal angle in degrees
+        """
+        return np.rad2deg(np.arctan2(self.j,self.i))
+    
+    def theta(self):
+        """
+        Calculates the polar angle in degrees
+        """
+        return np.rad2deg(np.arcsin(self.k/self.magnitude()))
+        
+    def __str__(self):
+        """
+        Assumes floating point when printing
+        """
+        r = self.r()
+        phi = self.phi()
+        theta = self.theta()
+        return f"PolarVector: ({r:.2f}, {theta:.2f}, {phi:.2f})"
+    
 X = Vector(1,0,0)
 Y = Vector(0,1,0)
 Z = Vector(0,0,1)
 
+X_polar = PolarVector(1, 0, 0)
+Y_polar = PolarVector(1, 0, -90)
+
 print(X-Y)
 print(Vector.cross(X, Z))
+print(X.magnitude())
+print(Y_polar)
