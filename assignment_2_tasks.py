22e43aeb34dddfb9f9f488b877151bb202beda5f
adam-rae <adam.rae@btinternet.com>
2025-02-18 21:21:17 +0000

    Update assignment_2_tasks.py

--- assignment_2_tasks.py
+++ assignment_2_tasks.py
@@ -24,7 +24,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from VectorClass import Vector, PolarVector
+from vector_class import Vector, PolarVector
 
 # ~~~~~~~~~~~~~~~~~~~~~~
 # Triangle 1 - Cartesian
