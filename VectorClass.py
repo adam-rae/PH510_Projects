5d463df22e5db2e4c922c522423ed1985105bf2e
adam-rae <adam.rae@btinternet.com>
2025-02-18 20:27:27 +0000

    Revert "More pylint changes"
    This reverts commit e1f6bd0aa0b311a606a5b6685209fdecab89274d.

--- VectorClass.py
+++ VectorClass.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 16 20:21:50 2025
+
+@author: adamr
+"""
+
+"""
+   Object oriented class of vectors
+"""
 import numpy as np
 
-"""
-Object oriented class of vectors working in Cartesian coordinates with adaptibility to polar coordinates
-
-"""
-
-class vector:
+class Vector:
     """
        Vector class for three dimensional quantities using Cartesian coordinates
     """
@@ -26,39 +30,38 @@ class vector:
         Overloads addition for the elements of two instances
 
         """
-        return vector(self.i + other.i, self.j + other.j, self.k + other.k)
-
+        return Vector(self.i + other.i, self.j + other.j, self.k + other.k)
+    
     def __sub__(self,other):
         """
         Overloads subtraction for the elements of two instances
 
         """
-        return vector(self.i - other.i, self.j - other.j, self.k - other.k)
-
+        return Vector(self.i - other.i, self.j - other.j, self.k - other.k)
+    
     def magnitude(self):
         """
-        Computes the magnitude of an instance as the square root of the sum of the squares of the
-        vector components
+        Computes the magnitude of an instance as the square root of the sum of the squares of the vector components
         
         """
         return np.sqrt(self.i**2 + self.j**2 + self.k**2)
-
+    
     def dot(self,other):
         """
         Computes the dot product of two vector instances
         
         """
         return self.i * other.i + self.j * other.j + self.k * other.k
-
+    
     def cross(self,other):
         """
         Computes the cross product of two vector instances
 
         """
-        return vector(self.j * other.k - self.k * other.j,
+        return Vector(self.j * other.k - self.k * other.j,
                       -(self.i * other.k - self.k * other.i),
                       self.i * other.j - self.j * other.i)
-
+    
     def area(self,other,other2):
         """
         Given 3 vertice instances, calculates the area of the triangle they create
@@ -66,67 +69,59 @@ class vector:
         """
         line_ab = other - self
         line_ac = other2 - self
-        return (vector.cross(line_ab, line_ac)).magnitude()/2
-
+        return (Vector.cross(line_ab, line_ac)).magnitude()/2
+    
     def angle(self,other):
         """
         Given two instances, calculates the angle between them
 
         """
-        return np.rad2deg(np.arccos(vector.dot(self,other)/ (self.magnitude() * other.magnitude())))
-
+        return np.rad2deg(np.arccos(Vector.dot(self,other)/ (self.magnitude() * other.magnitude())))
+    
     def angle_vertices(self,other,other2):
         """
         Taking 3 vertice points, caclulates the internal angles of the triangle they create
-        
         """
         line_ab, line_ac = other - self, other2 - self
         line_ba, line_bc = self - other, other2 - other
         line_ca, line_cb = self - other2, other - other2
-        return vector.angle(line_ab, line_ac),
-        vector.angle(line_bc, line_ba),
-        vector.angle(line_cb, line_ca)
-
-class polar_vector(vector):
-    """
-    Vector class for three dimensional quantities using spherical/polar coordinates in degrees
+        return Vector.angle(line_ab, line_ac), Vector.angle(line_bc, line_ba), Vector.angle(line_cb, line_ca)
     
+class PolarVector(Vector):
+    """
+       Vector class for three dimensional quantities using spherical/polar coordinates in degrees
     """
     def __init__(self, r, theta, phi):
         theta = np.deg2rad(theta)
         phi = np.deg2rad(phi)
-        vector.__init__(self,
+        Vector.__init__(self,
                         r*np.sin(theta) * np.cos(phi),
                         r*np.sin(theta) * np.sin(phi),
                         r*np.cos(theta))
-
+        
     def r(self):
         """
         Computes the radius component
-        
         """
         return self.magnitude()
-
+    
     def phi(self):
         """
         Computes the azimuthal angle in degrees
-        
         """
         return np.rad2deg(np.arctan2(self.j,self.i))
-
+    
     def theta(self):
         """
         Calculates the polar angle in degrees
-        
         """
         return np.rad2deg(np.arcsin(self.k/self.magnitude()))
-
+        
     def __str__(self):
         """
         Assumes floating point when printing and converts from Cartesian to polar
-        
         """
         r = self.r()
         phi = self.phi()
         theta = self.theta()
-        return f"polar_vector: ({r:.2f}, {theta:.2f}, {phi:.2f})"
+        return f"PolarVector: ({r:.2f}, {theta:.2f}, {phi:.2f})"
